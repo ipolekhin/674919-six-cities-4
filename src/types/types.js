c740e7ab058e6bc05fc42c77f1d51b3901627901
ipolekhin <ipolekhin@yadnex.ru>
2020-06-23 15:19:41 +0300

    3.1.2 В стейте компонента записывается активная карточка

--- src/types/types.js
+++ src/types/types.js
@@ -2,6 +2,18 @@ import PropTypes from "prop-types";
 
 const countPlacesType = PropTypes.number.isRequired;
 
+const handleHoverType = PropTypes.func.isRequired;
+
+const placeCardType = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  premiumPlace: PropTypes.bool.isRequired,
+  price: PropTypes.number.isRequired,
+  cardName: PropTypes.string.isRequired,
+  cardType: PropTypes.string.isRequired,
+  cardRating: PropTypes.string.isRequired,
+}).isRequired;
+
 const placeCardsType = PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
@@ -17,7 +29,9 @@ const placeCardsType = PropTypes.arrayOf(
 const titleClickType = PropTypes.func.isRequired;
 
 export {
+  placeCardType,
   countPlacesType,
+  handleHoverType,
   placeCardsType,
   titleClickType,
 };
