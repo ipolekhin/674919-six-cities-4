c228f69a741964f4d45240342c0de279d086b441
ipolekhin <ipolekhin@yadnex.ru>
2020-07-18 12:09:50 +0300

    5.0.2 Испрлавния по замечаниям наставника - использование цикла для вывода stars

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -8,6 +8,7 @@ import {OfferCardsClassesType} from "../../const";
 const Offer = (props) => {
   const {cityCoordinate, offer, placeCards, onTitleClick} = props;
   const nearPlaces = placeCards.filter((place) => offer.id !== place.id);
+  const FIVE_STAR = [5, 4, 3, 2, 1];
 
   return (
     <React.Fragment>
@@ -113,40 +114,20 @@ const Offer = (props) => {
                 <form className="reviews__form form" action="#" method="post">
                   <label className="reviews__label form__label" htmlFor="review">Your review</label>
                   <div className="reviews__rating-form form__rating">
-                    <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-                    <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-                    <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-                    <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-                    <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-                    <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
+                    {
+                      FIVE_STAR.map((value) => {
+                        return (
+                          <React.Fragment key={value}>
+                            <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio" />
+                            <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title="perfect">
+                              <svg className="form__star-image" width="37" height="33">
+                                <use xlinkHref="#icon-star"></use>
+                              </svg>
+                            </label>
+                          </React.Fragment>
+                        );
+                      })
+                    }
                   </div>
 
                   <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
