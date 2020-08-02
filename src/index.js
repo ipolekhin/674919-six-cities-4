2b7158cda1592bb447ff326b584cb502db642192
ipolekhin <ipolekhin@yadnex.ru>
2020-08-02 23:13:59 +0300

    7.1.7 Вернём поддержку Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {applyMiddleware, createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 import reducer from "./reducer/reducer.js";
@@ -12,9 +13,13 @@ import {createApi} from "./api.js";
 const api = createApi(() => {});
 const store = createStore(
     reducer,
-    applyMiddleware(thunk.withExtraArgument(api))
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
     // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
 );
+
+store.dispatch(DataOperation.getOffersList());
 
 ReactDOM.render(
     <Provider
