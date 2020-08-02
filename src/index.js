f7d953571f7e0c2362b6c7a54129b3c6e67db44f
ipolekhin <ipolekhin@yadnex.ru>
2020-08-02 22:36:39 +0300

    7.1.6 Подключим декомпозированные редьюсеры и Thunk в приложение

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {Provider} from "react-redux";
-import {reducer} from "./reducer/reducer.js";
+import thunk from "redux-thunk";
+import reducer from "./reducer/reducer.js";
 import App from "./components/app/app.jsx";
+import {Operation as DataOperation} from "./reducer/data/data.js";
+import {ActionCreator as SiteOperation} from "./reducer/site/site.js";
+import {createApi} from "./api.js";
 
+const api = createApi(() => {});
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
+    // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
 );
 
 ReactDOM.render(
--- src/reducer/data/data.test.js
+++ src/reducer/data/data.test.js
@@ -1,5 +1,8 @@
-import {reducer, placeCards} from "./data.js";
+import {reducer, placeCards, ActionType, Operation} from "./data.js";
+import MockAdapter from "axios-mock-adapter";
+import {createAPI} from "../../api.js";
 
+const api = createAPI(() => {});
 const city = `Amsterdam`;
 
 it(`Reducer without additional parameters should return initial state`, () => {
@@ -7,4 +10,23 @@ it(`Reducer without additional parameters should return initial state`, () => {
     offers: placeCards,
     offersOfTown: placeCards.filter((place) => place.townName === city),
   });
+});
+
+it(`Should make a correct API call to /hotels`, function () {
+  const apiMock = new MockAdapter(api);
+  const dispatch = jest.fn();
+  const offersList = Operation.getOffersList();
+
+  apiMock
+    .onGet(`/hotels`)
+    .reply(200, [{fake: true}]);
+
+  return offersList(dispatch, () => {}, api)
+    .then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: ActionType.OFFERS_LIST,
+        payload: [{fake: true}],
+      });
+    });
 });
