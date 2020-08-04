704c099c697fe42eee87e5a25d5f59d3a67d82bd
ipolekhin <ipolekhin@yadnex.ru>
2020-08-05 00:16:06 +0300

    7.2.3 Создаём в reducer/user/user.js action, action creator, функцию-reducer + тесты

--- src/reducer/user/user.js
+++ src/reducer/user/user.js
@@ -1,5 +1,33 @@
 import {AuthorizationStatus} from "../../const";
+import {extend} from "../../utils/common";
 
 const initialState = {
   authorizationStatus: AuthorizationStatus.NO_AUTH,
 };
+
+const ActionType = {
+  REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+};
+
+const ActionCreator = {
+  requireAuthorization: (status) => {
+    return ({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: status,
+    });
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRED_AUTHORIZATION:
+      return extend(state, {
+        authorizationStatus: action.payload,
+      });
+
+    default:
+      return state;
+  }
+};
+
+export {ActionCreator, ActionType, reducer};
---
+++ src/reducer/user/user.test.js
@@ -0,0 +1,64 @@
+import {ActionType, ActionCreator, reducer} from "./user.js";
+
+const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
+};
+
+describe(`Reducer User Test`, () => {
+  it(`Reducer without additional parameters should return initial state`, () => {
+    expect(reducer(void 0, {})).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+  });
+
+  it(`Reducer should change authorizationStatus by a given value`, () => {
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+  });
+
+  it(`Action creator for require authorization returns correct action`, () => {
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    });
+  });
+});
