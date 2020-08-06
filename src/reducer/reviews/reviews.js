48747d603ee7575833bf3a94bf04ae49e91f5d61
ipolekhin <ipolekhin@yadnex.ru>
2020-08-06 15:44:16 +0300

    7.3.3.3 Создаём в reducer/reviews/reviews.js operation для получения отзывов и публикации отзыва

--- src/reducer/reviews/reviews.js
+++ src/reducer/reviews/reviews.js
@@ -6,6 +6,7 @@ const initialState = {
 
 const ActionType = {
   ADD_REVIEW: `ADD_REVIEW`,
+  GET_REVIEWS: `GET_REVIEWS`,
 };
 
 const ActionCreator = {
@@ -15,11 +16,50 @@ const ActionCreator = {
       payload: value,
     });
   },
+  getReviews: (value) => {
+    return ({
+      type: ActionType.ADD_REVIEW,
+      payload: value,
+    });
+  },
+};
+
+const Operation = {
+  getReviews: (offerId) => (dispatch, getState, api) => {
+    return api.get(`/comments/${offerId}`)
+      .then((response) => {
+        dispatch(ActionCreator.getReviews(response.data));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  },
+  postReview: (offerId, postReview) => (dispatch, getState, api) => {
+    // console.log(`Отправка на сервер, начало`);
+    // console.log(offerId);
+    // console.log(postReview);
+
+    return api.post(`/comments/${offerId}`, {
+      postReview
+    })
+      // .then((response) => {
+      .then(() => {
+        // console.log(`Отправка на сервер`);
+        // dispatch(ActionCreator.addReview(response.data));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  },
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.ADD_REVIEW:
+      return extend(state, {
+        reviews: action.payload,
+      });
+    case ActionType.GET_REVIEWS:
       return extend(state, {
         reviews: action.payload,
       });
