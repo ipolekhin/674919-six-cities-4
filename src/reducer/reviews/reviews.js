52d6673ea9943e1b74dbcb1bcfffb5fc13fdb5cb
ipolekhin <ipolekhin@yadnex.ru>
2020-08-06 15:37:22 +0300

    7.3.3.2 Создаём в reducer/reviews/reviews.js action, action creator и функцию-reducer

--- src/reducer/reviews/reviews.js
+++ src/reducer/reviews/reviews.js
@@ -1,3 +1,32 @@
+import {extend} from "../../utils/common.js";
+
 const initialState = {
   reviews: [],
 };
+
+const ActionType = {
+  ADD_REVIEW: `ADD_REVIEW`,
+};
+
+const ActionCreator = {
+  addReview: (value) => {
+    return ({
+      type: ActionType.ADD_REVIEW,
+      payload: value,
+    });
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.ADD_REVIEW:
+      return extend(state, {
+        reviews: action.payload,
+      });
+
+    default:
+      return state;
+  }
+};
+
+export {ActionCreator, ActionType, Operation, reducer};
